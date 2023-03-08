209ce35594146c88ecdf8d845270f8a4ec01686f
alessandro miele <alessandro.miele@decathlon.com>
2023-03-08 17:39:20 +0100

    feat: added override copy fn, fix import fn, write override.json fn

--- bin/override.js
+++ bin/override.js
@@ -1,7 +1,86 @@
+//import
+require('dotenv').config(); // load env variable
+var fs = require('fs');
+
+//const
+const PATH_SPLITTER = "src";
+const RED_LOG = "\x1b[41m";
+const GREEN_LOG = "\x1b[42m";
+const NFS_IMPORT_PATH = "@nfs/ecommerce-front-core/src/"
+
+//vars
 const corePath = process.argv[2];
+const coreFileName = corePath.split("/").pop();
+const itFileName = coreFileName.split(".").join(`.${process.env.LOCALE}.`);
+const destinationPath = `${process.env.OVERRIDE_DESTINATION}${corePath.split(PATH_SPLITTER)[1].replace(coreFileName, itFileName)}`;
 const overrideComment = process.argv[3];
+const jiraTask = process.argv[4];
 
-console.log(corePath);
-console.log(overrideComment);
+// add override to override.json
+const overrideJson = async () => {
+    const overrideTemplate = () => {
+        try {
+            return JSON.parse(
+                `{
+                    "path": "${PATH_SPLITTER}${corePath.split(PATH_SPLITTER)[1]}",
+                    "override": "${PATH_SPLITTER}${destinationPath.split(PATH_SPLITTER)[1]}",
+                    "reasons": [
+                            {
+                                "name": "${overrideComment}",
+                                ${jiraTask ? `"jira": "${jiraTask}"` : ""}
+                            }
+                        ]
+                }`)
+        } catch (error) {
+            return error
+        }
+    
+    };
+    const jsonOverridePath = `${process.env.OVERRIDE_DESTINATION.split("/")[0]}/.migrate/override.json`;
+    try{
+        const jsonOverrideContent = await fs.promises.readFile(jsonOverridePath);
+        const jsonOverride = JSON.parse(jsonOverrideContent.toString());
+        jsonOverride.push(overrideTemplate());
+        await fs.promises.writeFile(jsonOverridePath, JSON.stringify(jsonOverride));
+        return true;
+    }
+    catch(e){
+        console.log(e);
+        return false;
+    }
+}
 
-console.log(process.env.OVERRIDE_SRC)
+// update import to copied file
+const updateImport = async (overridedFile) =>{
+    try{
+        let fileContent = await fs.promises.readFile(overridedFile);
+        let result = fileContent.toString()
+        .replaceAll("from '@/", `from '${NFS_IMPORT_PATH}`) // set core import
+        .replaceAll("from './", `from '${NFS_IMPORT_PATH}${corePath.split(PATH_SPLITTER)[1].replace(coreFileName, "")}`) // set relative import to core import
+        .replaceAll("@import '.", `@import '${NFS_IMPORT_PATH}${corePath.split(PATH_SPLITTER)[1].replace(coreFileName, "")}`) // replace .less import
+        await fs.promises.writeFile(overridedFile, result);
+        return true;
+    }
+    catch(e){
+        console.log(e);
+        return false;
+    }    
+}   
+
+// add override comment to file
+const addOvverideComment = async (overridedFile) => {
+    // TODO
+}
+
+//start
+(async () => {
+    try{
+        await fs.promises.cp(corePath, destinationPath,{recursive: true}); // copy file
+        await updateImport(destinationPath); // update import to copied file
+        await overrideJson(); // add to override.json
+    }
+    catch(e){
+        console.log(e);
+        return false;
+    };
+})();
--- bin/start.js
+++ bin/start.js
@@ -2,26 +2,28 @@
 const { exec } = require("child_process");
 var readline = require('readline');
 
+const RED_LOG = "\x1b[41m";
+
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-const ask = async () => {
+(async () => {
     let corePath = await new Promise(resolve => {rl.question("Insert core component path: ", input => resolve(input))});
     let overrideComment = await new Promise(resolve => {rl.question("Insert override comment: ", input => resolve(input))});
-
+    let jiraTask = await new Promise(resolve => {rl.question("Insert jira task: ", input => resolve(input))});
+    
     //good input
     if(corePath && overrideComment){
         rl.close();
-        exec(`node ${__dirname}/override.js "${corePath}" "${overrideComment}"`, (e, stdout, stderr) => {
+        exec(`node ${__dirname}/override.js "${corePath}" "${overrideComment}" "${jiraTask}"`, (e, stdout, stderr) => {
             if (e) return false;
             if (stderr) return false;
             console.log(stdout);
         });
         return true;
     }
+    
     // wrong input
-    console.log("You must provide valid path and comment");
+    console.log(RED_LOG, "You must provide valid path and comment");
     rl.close();
     return false;
-}
-
-ask();
+})()
